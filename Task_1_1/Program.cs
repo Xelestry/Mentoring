1e2ebcb90d0b538e974955964448c4dafd1ac9ea
Kirill Mityushchenko <Kirill_Mityushchenko@epam.com>
2020-07-14 21:21:04 +0600

    Finished .net core, winforms

---
+++ Task_1_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Task_1_3;
+
+namespace Task_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Please, enter your name: ");
+            // var userName = Console.ReadLine();
+            // Console.WriteLine($"Hello, {userName}");
+            // Console.ReadKey();
+
+            Console.WriteLine("Please, enter your name: ");
+            Console.WriteLine(HelloLibrary.SayHello(Console.ReadLine()));
+            Console.ReadKey();
+        }
+    } 
+}
